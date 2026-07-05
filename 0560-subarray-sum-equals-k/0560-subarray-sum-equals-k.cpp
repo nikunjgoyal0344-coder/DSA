938937041de5478c48007b74fbba5f8d7a58eb3a
nikunjgoyal0344-coder <nikunjgoyal0344@gmail.com>
2026-07-06 01:33:03 +0530

    Time: 39 ms (72.46%), Space: 46.9 MB (33.41%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,13 +2,35 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
+                               // brutefroce method
+        // int count = 0 ;
+        // for( int st = 0 ; st < n ; st++){
+        //     int sum = 0 ;
+        //     for( int end = st ; end < n ; end++){
+        //         sum+=nums[end];
+        //         if(sum == k ){count++;}
+        //     }
+        // }
+        // return count;
         int count = 0 ;
-        for( int st = 0 ; st < n ; st++){
-            int sum = 0 ;
-            for( int end = st ; end < n ; end++){
-                sum+=nums[end];
-                if(sum == k ){count++;}
+        vector<int> prearrSum(n,0);
+        // to find the arr of holding sums of privious one 
+        prearrSum[0] = nums[0];
+        for( int i = 1 ; i < n ; i++ ){
+            prearrSum[i] = prearrSum[i-1] + nums[i];
+        }
+        // make a unordered map to store the freq of ans and then add them to our count 
+        unordered_map<int,int> m;
+        for(int j = 0 ; j < n ; j++){
+            if(prearrSum[j] == k){
+                count++;
             }
+            int val = prearrSum[j] - k;
+            if(m.find(val)!= m.end()){
+                count += m[val];
+            }
+            m[prearrSum[j]]++;
+            
         }
         return count;
     }
